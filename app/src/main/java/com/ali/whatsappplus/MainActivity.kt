89795f46b252fa8cef8415dfcc0ec546618cdc57
Mohd Ashfaq <tbeast305@gmail.com>
2024-03-09 12:32:36 +0530

    tabs showing

--- app/src/main/java/com/ali/whatsappplus/MainActivity.kt
+++ app/src/main/java/com/ali/whatsappplus/MainActivity.kt
@@ -2,33 +2,42 @@ package com.ali.whatsappplus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
 import com.ali.whatsappplus.databinding.ActivityMainBinding
 import com.ali.whatsappplus.ui.adapter.MainViewPagerAdapter
 import com.ali.whatsappplus.ui.fragment.CallsFragment
 import com.ali.whatsappplus.ui.fragment.CommunitiesFragment
 import com.ali.whatsappplus.ui.fragment.OngoingChatsFragment
 import com.ali.whatsappplus.ui.fragment.UpdatesFragment
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var adapter: MainViewPagerAdapter
+    private lateinit var tabLayout: TabLayout
+    private lateinit var viewPager: ViewPager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setUpTabs()
+    }
+
+    private fun setUpTabs() {
+        tabLayout = findViewById(R.id.tab_layout)
+        viewPager = findViewById(R.id.view_pager)
 
         adapter = MainViewPagerAdapter(supportFragmentManager)
 
-        // Dynamically add tabs
         adapter.addFragment(CommunitiesFragment(), "Communities")
         adapter.addFragment(OngoingChatsFragment(), "Chats")
         adapter.addFragment(UpdatesFragment(), "Updates")
         adapter.addFragment(CallsFragment(), "Calls")
         adapter.notifyDataSetChanged()
 
-        binding.viewPager.adapter = adapter
-        binding.tabLayout.setupWithViewPager(binding.viewPager)
+        viewPager.adapter = adapter
+        tabLayout.setupWithViewPager(viewPager)
     }
 }
