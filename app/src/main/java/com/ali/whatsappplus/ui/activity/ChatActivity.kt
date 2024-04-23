79bb819bc2f424868097384de6c8eb2318a445f2
ashfaaqali <tbeast305@gmail.com>
2024-04-23 17:18:19 +0530

    migrate to photo picker

--- app/src/main/java/com/ali/whatsappplus/ui/activity/ChatActivity.kt
+++ app/src/main/java/com/ali/whatsappplus/ui/activity/ChatActivity.kt
@@ -60,6 +60,7 @@ class ChatActivity : AppCompatActivity() {
     private var latestReceivedMessageId = 0
     private lateinit var bottomSheetDialog: AttachmentsBottomSheet
 
+
     private val mTextEditorWatcher: TextWatcher = object : TextWatcher {
         override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
         override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
