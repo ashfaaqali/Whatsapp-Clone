95bfc62e4e169d6ac75eae46028d8f1187e16c86
ashfaaqali <tbeast305@gmail.com>
2024-04-23 11:10:59 +0530

    permission setup

--- app/src/main/java/com/ali/whatsappplus/ui/bottomsheet/AttachmentsBottomSheet.kt
+++ app/src/main/java/com/ali/whatsappplus/ui/bottomsheet/AttachmentsBottomSheet.kt
@@ -1,22 +1,30 @@
 package com.ali.whatsappplus.ui.bottomsheet
 
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.Manifest.permission.READ_MEDIA_IMAGES
+import android.Manifest.permission.READ_MEDIA_VIDEO
+import android.Manifest.permission.READ_MEDIA_VISUAL_USER_SELECTED
 import android.app.Dialog
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
 import com.ali.whatsappplus.R
 import com.ali.whatsappplus.databinding.AttachmentsBottomSheetBinding
-import com.ali.whatsappplus.databinding.ChatItemBinding
-import com.ali.whatsappplus.databinding.SampleLoginBottomSheetBinding
 import com.ali.whatsappplus.ui.activity.ChatActivity
-import com.ali.whatsappplus.ui.activity.MainActivity
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class AttachmentsBottomSheet : BottomSheetDialogFragment() {
     private lateinit var binding: AttachmentsBottomSheetBinding
-
+    private var TAG = "AttachmentsBottomSheet"
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -25,42 +33,60 @@ class AttachmentsBottomSheet : BottomSheetDialogFragment() {
         return binding.root
     }
 
+    // Register ActivityResult handler
+    private val requestPermissions =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { results ->
+            Log.d(TAG, "Permission Result: $results")
+        }
+
+    @RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val dialog = super.onCreateDialog(savedInstanceState) as BottomSheetDialog
         dialog.window?.attributes?.windowAnimations = R.style.BottomSheetAnimation
         binding = AttachmentsBottomSheetBinding.inflate(layoutInflater)
 
-        binding.document.setOnClickListener{
-            (activity as? MainActivity)?.logoutAndLogin("superhero1")
-            dismiss()
-        }
-
-        binding.camera.setOnClickListener{
-            (activity as? MainActivity)?.logoutAndLogin("superhero2")
-            dismiss()
-        }
-
-        binding.gallery.setOnClickListener{
-            (activity as? ChatActivity)?.openImagePicker()
-            dismiss()
-        }
-
-        binding.audio.setOnClickListener{
-            (activity as? MainActivity)?.logoutAndLogin("superhero4")
-            dismiss()
-        }
-
-        binding.location.setOnClickListener{
-            (activity as? MainActivity)?.logoutAndLogin("superhero4")
-            dismiss()
-        }
-
-        binding.contacts.setOnClickListener{
-            (activity as? MainActivity)?.logoutAndLogin("superhero4")
-            dismiss()
+        binding.gallery.setOnClickListener {
+            if (!isMediaPermissionGranted()) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) requestPermissions.launch(
+                    arrayOf(READ_MEDIA_IMAGES, READ_MEDIA_VIDEO, READ_MEDIA_VISUAL_USER_SELECTED)
+                )
+                else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) requestPermissions.launch(
+                    arrayOf(READ_MEDIA_IMAGES, READ_MEDIA_VIDEO)
+                )
+                else requestPermissions.launch(arrayOf(READ_EXTERNAL_STORAGE))
+            } else {
+                (activity as? ChatActivity)?.openImagePicker()
+                dismiss()
+            }
         }
 
         return dialog
+    }
+
+    // Checks If A Permission Is Granted And Returns Boolean
+    @RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
+    private fun isMediaPermissionGranted(): Boolean {
+
+        // Permissions
+        val readExternalStoragePermission =
+            ContextCompat.checkSelfPermission(requireContext(), READ_EXTERNAL_STORAGE)
+        val readMediaImagesPermission =
+            ContextCompat.checkSelfPermission(requireContext(), READ_MEDIA_IMAGES)
+        val readMediaVideoPermission =
+            ContextCompat.checkSelfPermission(requireContext(), READ_MEDIA_VIDEO)
+        val readUserSelectedMediaPermission =
+            ContextCompat.checkSelfPermission(requireContext(), READ_MEDIA_VISUAL_USER_SELECTED)
+
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU && (readMediaImagesPermission == PERMISSION_GRANTED || readMediaVideoPermission == PERMISSION_GRANTED)) {
+            true
+        } else if (
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE && readUserSelectedMediaPermission == PERMISSION_GRANTED) {
+            true
+        } else if (readExternalStoragePermission == PERMISSION_GRANTED) {
+            true
+        } else {
+            false
+        }
     }
 
     override fun getTheme() = R.style.Theme_BottomSheetDialog
